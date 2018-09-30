cd3adb74cfb32b2484129b409e651f7ef4899386
Chase Jones <cdjones03@email.wm.edu>
2018-09-30 17:35:45 -0400

    8th
    Implemented method to check if two mazes, generating deterministically,
    are equal (based on distance comparison).

--- MazeByChaseJones/test/generation/MazeFactoryTest.java
+++ MazeByChaseJones/test/generation/MazeFactoryTest.java
@@ -58,11 +58,32 @@ public class MazeFactoryTest {
 	@Test
 	public void reproduceMaze()
 	{
+		boolean check = true;
 		StubOrder stub2 = new StubOrder(Builder.DFS, 0, true);
 		mazeFac.order(stub2);
 		mazeFac.waitTillDelivered();
 		MazeConfiguration config2 = stub2.getMazeConfiguration();
-		assertEquals(config, config2);
+		
+		Distance dist, dist2;
+		dist = config.getMazedists();
+		dist2 = config2.getMazedists();
+		
+		for(int x = 0; x < config.getWidth(); x++)
+		{
+			for(int y = 0; y < config.getHeight(); y++)
+			{
+				if(dist.getDistanceValue(x,y) == dist2.getDistanceValue(x,y))
+					continue;
+				else
+				{
+					check = false;
+					break;
+				}
+			}
+			if(check)
+				break;
+		}
+		assertEquals(true, check);
 	}
 
 }
