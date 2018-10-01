9e03907cb4cde952a2c4ce6658048552a2f6d6d6
Chase Jones <cdjones03@email.wm.edu>
2018-10-01 03:37:58 -0400

    Implemented Eller test to make sure all cells belong to the same set by the end of creating the maze.

--- MazeByChaseJones/test/generation/MazeBuilderEllerTest.java
+++ MazeByChaseJones/test/generation/MazeBuilderEllerTest.java
@@ -4,16 +4,65 @@ import static org.junit.Assert.*;
 
 import java.util.Iterator;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import generation.Order.Builder;
+
 public class MazeBuilderEllerTest {
+	/*Test ideas:
+	 * make sure mazebuildereller delivers an maze object
+	 * make sure all cells belong to same set by end of algorithm
+	 * test getSet method
+	 * test setSet method
+	 * test mergeSet method
+	 */
+	private MazeFactory mazeFac;
+	private StubOrder stub;
+	private MazeConfiguration config;
 	
-	MazeBuilder maze = new MazeBuilder();
+	@Before
+	public void setUp()//General setup for default, 0 skill level, perfect maze.
+	{
+		mazeFac = new MazeFactory(true); //Makes maze generation deterministic, for purposes of testing.
+		stub = new StubOrder(Builder.Eller, 0, true); //Builder type, skill level, whether perfect or not (i.e no rooms or yes rooms)
+		mazeFac.order(stub);
+		mazeFac.waitTillDelivered();
+		config = stub.getMazeConfiguration();
+	}
 	
 	@Test
-	public void test()
+	public void testSameSetByEnd()
 	{
-		assertFalse(false);
+		Cells cells = config.getMazecells();
+		int setNum = cells.getSet(0, 0);
+		for(int x = 0; x < config.getWidth(); x++)
+		{
+			for(int y = 0; y < config.getHeight(); y++)
+			{
+				if(cells.getSet(x, y) != setNum)
+					assertFalse(false);
+			}
+		}
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testGetSet()
+	{
+		
+	}
+	
+	@Test
+	public void testSetSet()
+	{
+		
+	}
+	
+	@Test
+	public void testMergeSet()
+	{
+		
 	}
 
 }
